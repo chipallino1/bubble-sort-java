bf8aae6652ad1e1e11d1d209532738ef60c1c28d
yskarupich_ext <yahor.skarupich_ext@payback.net>
2021-10-26 13:08:54 +0200

    Swap operation moved to separated method.

--- src/main/java/com/udemy/bubblesort/Main.java
+++ src/main/java/com/udemy/bubblesort/Main.java
@@ -5,26 +5,30 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arraysOfInts = new Random().ints(100, -100, 100).toArray();
+        int[] arrayOfInts = new Random().ints(10, -100, 100).toArray();
 
-        System.out.println(Arrays.toString(arraysOfInts));
+        System.out.println(Arrays.toString(arrayOfInts));
 
-        int unsortedPartitionIndex = arraysOfInts.length - 1;
+        int unsortedPartitionIndex = arrayOfInts.length - 1;
 
         for (int i = 0; i < unsortedPartitionIndex + 1; i++) {
             if (i == unsortedPartitionIndex) {
                 i = 0;
                 unsortedPartitionIndex--;
             }
-            if (arraysOfInts[i] > arraysOfInts[i + 1]) {
-                // swapping elements without temp variable
-                // a = 15
-                // b = 3
-                arraysOfInts[i + 1] = arraysOfInts[i + 1] - arraysOfInts[i]; // b = b - a; calculate diff; b = 3 - 15 = -12;
-                arraysOfInts[i] = arraysOfInts[i] + arraysOfInts[i + 1]; // a = a + b; add diff to a; a = 15 + (-12) = 3;
-                arraysOfInts[i + 1] = arraysOfInts[i] - arraysOfInts[i + 1]; // b = a - b; subtract diff from a; b = 3 - (-12) = 15;
+            if (arrayOfInts[i] > arrayOfInts[i + 1]) {
+                swapWithNextOne(arrayOfInts, i);
             }
         }
-        System.out.println(Arrays.toString(arraysOfInts));
+        System.out.println(Arrays.toString(arrayOfInts));
+    }
+
+    private static void swapWithNextOne(int[] arrayOfInts, int i) {
+        // swapping elements without temp variable
+        // a = 15
+        // b = 3
+        arrayOfInts[i + 1] = arrayOfInts[i + 1] - arrayOfInts[i]; // b = b - a; calculate diff; b = 3 - 15 = -12;
+        arrayOfInts[i] = arrayOfInts[i] + arrayOfInts[i + 1]; // a = a + b; add diff to a; a = 15 + (-12) = 3;
+        arrayOfInts[i + 1] = arrayOfInts[i] - arrayOfInts[i + 1];
     }
 }
